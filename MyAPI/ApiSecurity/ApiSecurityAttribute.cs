cfc2ce16938d03c0c17b59be1ca294d5dde847a2
devide <devide@126.com>
2019-03-18 09:32:57 +0800

    优化

--- MyAPI/ApiSecurity/ApiSecurityAttribute.cs
+++ MyAPI/ApiSecurity/ApiSecurityAttribute.cs
@@ -13,36 +13,30 @@ namespace MyAPI.ApiSecurity
     {
         public override void OnAuthorization(HttpActionContext actionContext)
         {
-            if (actionContext.Request.Method.Method != "OPTIONS")
+            //从http请求的头里面获取身份验证信息，验证是否是请求发起方的ticket
+            var authorization = actionContext.Request.Headers.Authorization;
+            if ((authorization != null) && (authorization.Scheme != null))
             {
-                //从http请求的头里面获取身份验证信息，验证是否是请求发起方的ticket
-                var authorization = actionContext.Request.Headers.Authorization;
-                if ((authorization != null) && (authorization.Scheme != null))
+                //解密用户ticket,并校验用户名密码是否匹配
+                var encryptTicket = authorization.Scheme;
+                if (ValidateTicket(encryptTicket))
                 {
-                    //解密用户ticket,并校验用户名密码是否匹配
-                    var encryptTicket = authorization.Scheme;
-                    if (ValidateTicket(encryptTicket))
-                    {
-                        base.IsAuthorized(actionContext);
-                    }
-                    else
-                    {
-                        HandleUnauthorizedRequest(actionContext);
-                    }
+                    base.IsAuthorized(actionContext);
                 }
-                //如果取不到身份验证信息，并且不允许匿名访问，则返回未验证401
                 else
                 {
-                    var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
-                    bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
-                    if (isAnonymous) base.OnAuthorization(actionContext);
-                    else HandleUnauthorizedRequest(actionContext);
+                    HandleUnauthorizedRequest(actionContext);
                 }
             }
+            //如果取不到身份验证信息，并且不允许匿名访问，则返回未验证401
             else
             {
-                base.IsAuthorized(actionContext);
+                var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
+                bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
+                if (isAnonymous) base.OnAuthorization(actionContext);
+                else HandleUnauthorizedRequest(actionContext);
             }
+
         }
 
         //校验用户名密码（正式环境中应该是数据库校验）
